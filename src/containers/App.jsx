bfb0bd962d670120f66fc56f8981794fdbb36de0
Eugene-Okhryamkin <eugene.okhryamkin@gmail.com>
2019-07-09 22:06:52 +0300

    first commit

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,12 +1,15 @@
 import React , { Component } from "react";
 import { connect } from "react-redux";
 import Sidebar from "./sidebar/sidebar.jsx";
+import Main from "./main/main.jsx";
+import "./App.sass";
 
 class App extends Component {
     render() {
         return(
             <>
                 <Sidebar />
+                <Main />
             </>
         )
     };
---
+++ src/containers/main/main.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./main.sass";
+
+const Main = () => {
+    return(
+        <div id="Main">
+            <h1>Главная</h1>
+        </div>
+    );
+};
+
+export default Main;
